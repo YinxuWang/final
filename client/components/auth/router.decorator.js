0d42b5f1958010f157030dde07fdc6829972c16c
byskywalker <byskywalker@gmail.com>
2016-12-07 21:46:09 +0800

    公司管理,权限重构

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -5,7 +5,6 @@ export function routerDecorator($rootScope, $state, Auth) {
   // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
 
   $rootScope.$on('$stateChangeStart', function(event, next) {
-    debugger;
     if(!next.authenticate) {
       return;
     }
