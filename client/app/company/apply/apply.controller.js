16e05c2193e9694995eda0c5681fa53d061cd753
byskywalker <byskywalker@gmail.com>
2016-12-08 00:16:47 +0800

    公司注册,公司架构

---
+++ client/app/company/apply/apply.controller.js
@@ -0,0 +1,6 @@
+export default class CompanyApplyController {
+
+  constructor() {
+    'ngInject';
+  }
+}
--- client/app/company/index.js
+++ client/app/company/index.js
@@ -2,7 +2,9 @@
 
 import angular from 'angular';
 import routes from './company.routes';
+import structure from './structure';
+import apply from './apply';
 
-export default angular.module('app.company',['app.auth','ui.router'])
+export default angular.module('app.company', ['app.auth', 'ui.router', structure, apply])
   .config(routes)
   .name;
